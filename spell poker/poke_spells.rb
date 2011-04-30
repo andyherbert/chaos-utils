704aa7fec5d1143596a0ccc81c234c4dc61650f2
Andy Herbert <andy.herbert@gmail.com>
2011-04-30 16:14:44 +0100

    added chaos.otf, an opentype font based on the chaos typeset

--- spell poker/poke_spells.rb
+++ spell poker/poke_spells.rb
@@ -2,8 +2,8 @@
 require '../chaos-lib/chaos'
 
 chaos = Chaos.new('chaos.sna')
-spells = ['disbelieve', 'raise dead', 'vampire', 'shadow wood', 'magic castle', 'lion', 'elf', 'crocodile', 'goblin', 'orc']
+spells = ['disbelieve', 'elf', 'king cobra', 'king cobra', 'king cobra', 'king cobra', 'king cobra', 'king cobra', 'king cobra', 'king cobra', 'king cobra', 'king cobra', 'king cobra']
 chaos.poke_spells(0, spells)
-chaos.poke_spells(1, spells)
+# chaos.poke_spells(1, spells)
 chaos.dump_snapshot('chaos_poked.sna')
 `open ./chaos_poked.sna`
